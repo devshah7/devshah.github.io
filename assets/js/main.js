f73f182ffea3dcd1be59976f480f4d347434b35a
Dev Shah <shahdev99@gmail.com>
2024-07-09 23:11:01 -0400

    skills-section updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -188,6 +188,30 @@
       }, true);
     }
 
+    let skillsContainer = select('.skills-container');
+    if (skillsContainer) {
+      let skillsIsotope = new Isotope(skillsContainer, {
+        itemSelector: '.skills-content'
+      });
+
+      let skillsFilters = select('#skills-flters li', true);
+
+      on('click', '#skills-flters li', function(e) {
+        e.preventDefault();
+        skillsFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        skillsIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        skillsIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
   });
 
   /**
